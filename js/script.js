7f51b47e5dc279e05a89e1e7ef07fc71e45a8b33
MaksimKhandeshyn <145330399+MaksimKhandeshyn@users.noreply.github.com>
2024-12-01 20:48:15 +0200

    make 1-4 tasks

---
+++ js/script.js
@@ -0,0 +1,27 @@
+//--1--
+// let number = 1;
+// while (number <= 10) {
+//   console.log(number);
+//   number++;
+// }
+//--2--
+// let i = 2;
+// for (; i <= 20; i++) {
+//   if (i % 2 !== 0) {
+//     continue;
+//   }
+//   console.log(i);
+// }
+// --3--
+// let a = 0;
+// for (; a <= 10; a++) {
+//   let sum = a * 7;
+//   console.log(`${a} * 7 = ${sum}`);
+// }
+// --4--
+let array = [1, 2, 3, 4, 5];
+let i = 1;
+while (i <= array) {
+  i++;
+  console.log(array);
+}
